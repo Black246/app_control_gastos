c5f6993b091b7be87fe6557bf4b9f37fe13bf9b9
MartinXdel <MartinXdel@MARTIN>
2023-10-17 22:45:50 -0500

    agregue la opcion de describir el gasto

--- expenses_app/lib/pages/add_expense.dart
+++ expenses_app/lib/pages/add_expense.dart
@@ -1,4 +1,5 @@
 import 'package:expenses_app/widgets/add_expenses_wt/bs_number_keyboard.dart';
+import 'package:expenses_app/widgets/add_expenses_wt/comments_box.dart';
 import 'package:flutter/material.dart';
 
 import '../utils/constans.dart';
@@ -8,31 +9,37 @@ class AddExpenses extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Agregar Gasto'),
-        elevation: 0.0,
-      ),
-      body: Column(
-        children: [
-          const BSNumberKeyboard(),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              decoration: Constants.sheetBoxDecoration(
-                  Theme.of(context).primaryColorDark),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text('Fecha 12/12/12'),
-                  Text('Seleccionar Categoria'),
-                  Text('Agregar Descripción'),
-                  Expanded(child: Center(child: Text('Botón Guardar')))
-                ],
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Agregar Gasto'),
+          elevation: 0.0,
+        ),
+        body: Column(
+          children: [
+            const BSNumberKeyboard(),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: Constants.sheetBoxDecoration(
+                    Theme.of(context).primaryColorDark),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text('Fecha 12/12/12'),
+                    Text('Seleccionar Categoria'),
+                    CommentBox(),
+                    Expanded(child: Center(child: Text('Botón Guardar')))
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
--- expenses_app/lib/widgets/add_expenses_wt/bs_number_keyboard.dart
+++ expenses_app/lib/widgets/add_expenses_wt/bs_number_keyboard.dart
@@ -13,6 +13,10 @@ class _BSNumberKeyboardState extends State<BSNumberKeyboard> {
 
   @override
   Widget build(BuildContext context) {
+    // funcion para detectar cuando hay mas de 3 digitod y pone una coma
+    String Function(Match) mathFunc;
+    RegExp reg = RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
+    mathFunc = (Match match) => '${match[1]},';
     return GestureDetector(
       onTap: () {
         _numPad();
@@ -23,7 +27,7 @@ class _BSNumberKeyboardState extends State<BSNumberKeyboard> {
           children: [
             const Text('Cantidad Ingresada'),
             Text(
-              '\$ $import',
+              '\$ ${import.replaceAllMapped(reg, mathFunc)}',
               style: const TextStyle(
                   fontSize: 30.0,
                   letterSpacing: 2.0,
@@ -67,99 +71,108 @@ class _BSNumberKeyboardState extends State<BSNumberKeyboard> {
             borderRadius: BorderRadius.vertical(top: Radius.circular(30.0))),
         context: context,
         builder: (BuildContext context) {
-          return SizedBox(
-            height: 350.0,
-            child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraints) {
-              var heigth = constraints.biggest.height / 5;
-              return Column(
-                children: [
-                  Table(
-                    border: TableBorder.symmetric(
-                        inside: const BorderSide(
-                            /*color: Colors.grey,*/ width: 0.1)),
-                    children: [
-                      TableRow(children: [
-                        number(
-                          '1',
-                          heigth,
-                        ),
-                        number('2', heigth),
-                        number('3', heigth)
-                      ]),
-                      TableRow(children: [
-                        number(
-                          '4',
-                          heigth,
-                        ),
-                        number('5', heigth),
-                        number('6', heigth)
-                      ]),
-                      TableRow(children: [
-                        number(
-                          '7',
-                          heigth,
-                        ),
-                        number('8', heigth),
-                        number('9', heigth)
-                      ]),
-                      TableRow(children: [
-                        number(
-                          '.',
-                          heigth,
-                        ),
-                        number('0', heigth),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              if (import.length > 0.0) {
-                                import = import.substring(0, import.length - 1);
-                              }
-                            });
-                          },
-                          behavior: HitTestBehavior.opaque,
-                          child: SizedBox(
-                            height: heigth,
-                            child: const Icon(
-                              Icons.backspace,
-                              size: 35.0,
-                            ),
+          return WillPopScope(
+            onWillPop: () async => false,
+            child: SizedBox(
+              height: 350.0,
+              child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                var heigth = constraints.biggest.height / 5;
+                return Column(
+                  children: [
+                    Table(
+                      border: TableBorder.symmetric(
+                          inside: const BorderSide(
+                              /*color: Colors.grey,*/ width: 0.1)),
+                      children: [
+                        TableRow(children: [
+                          number(
+                            '1',
+                            heigth,
                           ),
-                        )
-                      ])
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: GestureDetector(
-                          child: Constants.customButtom(
-                              Colors.transparent, Colors.red, 'Cancelar'),
-                          onTap: () {
-                            setState(() {
-                              import = '0.00';
-                              Navigator.pop(context);
-                            });
-                          },
+                          number('2', heigth),
+                          number('3', heigth)
+                        ]),
+                        TableRow(children: [
+                          number(
+                            '4',
+                            heigth,
+                          ),
+                          number('5', heigth),
+                          number('6', heigth)
+                        ]),
+                        TableRow(children: [
+                          number(
+                            '7',
+                            heigth,
+                          ),
+                          number('8', heigth),
+                          number('9', heigth)
+                        ]),
+                        TableRow(children: [
+                          number(
+                            '.',
+                            heigth,
+                          ),
+                          number('0', heigth),
+                          GestureDetector(
+                            behavior: HitTestBehavior.opaque,
+                            onTap: () {
+                              setState(() {
+                                if (import.length > 0.0) {
+                                  import =
+                                      import.substring(0, import.length - 1);
+                                }
+                              });
+                            },
+                            onLongPress: () {
+                              setState(() {
+                                import = '';
+                              });
+                            },
+                            child: SizedBox(
+                              height: heigth,
+                              child: const Icon(
+                                Icons.backspace,
+                                size: 35.0,
+                              ),
+                            ),
+                          )
+                        ])
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: GestureDetector(
+                            child: Constants.customButtom(
+                                Colors.transparent, Colors.red, 'Cancelar'),
+                            onTap: () {
+                              setState(() {
+                                import = '0.00';
+                                Navigator.pop(context);
+                              });
+                            },
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        child: GestureDetector(
-                          child: Constants.customButtom(
-                              Colors.transparent, Colors.green, 'Aceptar'),
-                          onTap: () {
-                            setState(() {
-                              if (import.length == 0.0) import = '0.00';
-                              Navigator.pop(context);
-                            });
-                          },
+                        Expanded(
+                          child: GestureDetector(
+                            child: Constants.customButtom(
+                                Colors.transparent, Colors.green, 'Aceptar'),
+                            onTap: () {
+                              setState(() {
+                                if (import.length == 0.0) import = '0.00';
+                                Navigator.pop(context);
+                              });
+                            },
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
-              );
-            }),
+                      ],
+                    )
+                  ],
+                );
+              }),
+            ),
           );
         });
   }
